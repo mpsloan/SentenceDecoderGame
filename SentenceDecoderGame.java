cdbac4729f734f2a91cdce1324fba0df6f48d863
mpsloan <97258032+mpsloan@users.noreply.github.com>
2022-01-06 17:01:26 -0500

    Update SentenceDecoderGame.java

--- SentenceDecoderGame.java
+++ SentenceDecoderGame.java
@@ -1,15 +1,5 @@
 /*
 Michael Sloan
-1180-02 / 1180L-06
-Wallace Neikirk / Luke Holt
-October 28, 2021
-This program is a sentence decoder game in which the program reads a file and chooses a
-random sentence from that file. Once it chooses that sentence it then encodes the specified
-sentence using a permuted alphabet. The program then outputs the sentence and constantly asks 
-the user if they would like to guess one letter, or the entire sentence. Once the user has had 
-enough letters revealed to guess the entire sentence, their number of correct and incorrect 
-guesses are displayed on the screen and they are given the option to play again.
-*/
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
